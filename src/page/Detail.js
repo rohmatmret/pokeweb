9d25acc52c8daadb6fd7f392a9e3123226d7121b
rohmatmret <rohmat771@gmail.com>
2021-01-18 23:33:33 +0700

    refactor to function component

--- src/page/Detail.js
+++ src/page/Detail.js
@@ -1,10 +1,13 @@
 import React, { useState, useEffect,useContext } from 'react';
-import { useLocation } from 'react-router-dom'
-import { Spinner,Row,Col,Card,Container,Badge,Button } from 'react-bootstrap'
+import { Redirect, useLocation } from 'react-router-dom'
 import "../App.css"
+import { Container,Row,Col } from 'react-bootstrap'
 import AppContext from '../AppContext.js'
+import CardLoading from '../components/Details/CardLoading'
+import CardMoves from '../components/Details/CardMoves'
+import CardImages from '../components/Details/Cardimages'
+import CardAbility from '../components/Details/CardAbility'
 const { CartContext } = AppContext;
-
 
 function useQuery() {
   return new URLSearchParams(useLocation().search);
@@ -21,17 +24,24 @@ const defaultProps = {
   base_experience:0,
   sprites:{},
   moves:[],
-  types:[]
+  types:[],
+  versions:[]
 }
 
 
 function Details () {
   let query = useQuery();
   let name  =  query.get('name')
+  const cart = window.localStorage.getItem('cart')
   const [pokemons, setPokemons] = useState(defaultProps)
   const [isLoading, setLoading]= useState(true)
   const apiUrl = 'https://pokeapi.co/api/v2/pokemon/';
   const props = useContext(CartContext)
+  const [collection, setCollection] = useState([])
+  const [isErrors ,setErrors] = useState(false)
+  const [actions ,setActions] = useState(false)
+  const [isSuccessCatch ,setSuccessCatch] = useState(false)
+  
   useEffect(()=>{
     document.title = 'Pokemon ' + query.get('name')
     const fetchData = async () =>{
@@ -40,7 +50,7 @@ function Details () {
         const data = await response.json()
         
         setPokemons(prevState => ({
-          ...data,           // copy all other field/objects
+          ...data,           
           id:data.id,
           name:data.name,
           url:data.url,
@@ -51,103 +61,92 @@ function Details () {
           sprites: data.sprites,
           moves: data.moves,
           types:data.types,
+          versions:data.sprites.versions
         }));
         setLoading(false)
+        console.log('setloadiing false')
       } catch (error) {
         setLoading(false)
-        console.log(error)
+        console.log('setloadiing false')
+        setErrors(true)
       }
     }
     
     fetchData()
-  },[ query,name])
+
+  },[])
+
+  useEffect(()=>{
+    const fetchCart = () =>{
+      if(cart){
+        let incart = JSON.parse(cart)
+        setCollection((e=>incart))
+      }
+    }
+    fetchCart()
+  },[cart])
+
+  if (isErrors) {
+    return <Redirect to='/' />
+  }
   const add = ()=> {
-    const cart = window.localStorage.getItem('cart')
-    if (cart) {
-      let data = {
-        name:pokemons.name,
-        id: pokemons.id,
-        url: pokemons.url
+    //success probability is 50%
+    setActions(true)
+    let isSuccess = Math.random() < 0.5 ? true : false;
+    if (isSuccess) {
+      
+      if (cart) {
+        let data = {
+          name:pokemons.name,
+          id: pokemons.id,
+          url: pokemons.url
+        }
+        let incart = JSON.parse(cart)
+  
+        if (incart){
+          incart.push(data)
+          window.localStorage.setItem('cart',JSON.stringify(incart))  
+          props.setCart(incart.length)
+        }
+        setSuccessCatch(true)
+      } else {
+        let data = [{
+          name: pokemons.name,
+          id: pokemons.id,
+          url: pokemons.url
+        }]
+        props.setCart(data.length)
+        window.localStorage.setItem('cart',JSON.stringify(data))
+        setSuccessCatch(true)
       }
-      let incart = JSON.parse(cart)
-
-      if (incart){
-        //incart.push(data)
-        console.log(typeof incart)
-        console.log('data cart' ,JSON.stringify(incart))
-        incart.push(data)
-        window.localStorage.setItem('cart',JSON.stringify(incart))  
-        props.setCart(incart.length)
-      }
-      // incart.push(data)
     } else {
-      let data = [{
-        name: pokemons.name,
-        id: pokemons.id,
-        url: pokemons.url
-      }]
-      props.setCart(data.length)
-      window.localStorage.setItem('cart',JSON.stringify(data))
-
+      setSuccessCatch(false)
     }
 
   }
 
+
   return (
     <Container>
-      <Row className="justify-content-md-center">
-        <Col lg={1}>
-          { isLoading ? <Spinner  animation="grow" /> : null}
-        </Col>
-      </Row>
+      <CardLoading isLoading={isLoading} />
       <Row>
         <Col lg={4}>
-          <Card className="cards-detail">
-            <img src={pokemons.sprites.front_default }  alt={pokemons.sprites.front_default } className="rounded detail-img-card"/>
-            <Card.Body>
-              <Card.Text>
-                <p className="h2 text-center">{pokemons.name} </p>
-              </Card.Text>
-              <Card.Text>
-              { pokemons.types.map((t,index) => {
-                return <Badge key={index}pill variant="primary" className="p-3 m-1">{t.type ? t.type.name :null} </Badge>
-              })}
-              </Card.Text>
-            </Card.Body>
-            
-            <Card.Body>
-            </Card.Body>
-          </Card>
+          <CardImages name={pokemons.name} sprites={pokemons.sprites} types={pokemons.types} collection={collection}/>
         </Col>
         <Col lg={3}>
-          <h4 className="h4">Ability</h4>
-            { pokemons.abilities.map((g,index) => {
-              return <Badge key={index} pill variant="light" className="p-2 m-1">{g.ability ? g.ability.name :null} </Badge>
-            })}
-          
+          <CardAbility abilities={pokemons.abilities}/>
         </Col>
-        <Col lg={4}>
-          <h4 className="h4">Moves</h4>
-            { pokemons.moves.map((m,index) => {
-              return <Badge key={index} pill variant="dark" className="p-2 m-1">
-                {m.move ? m.move.name :null}
-              </Badge>
-            })}
-        </Col>
-        {/* <Col>
-          <h4 className="h4">Experiences</h4>
-          <span className="score"> { pokemons.base_experience } </span>
-        </Col> */}
       </Row>
       <Row>
-        <div className="button_bottom">
-          <Button className="btn" onClick={add}> 
-            <img src="/icons8-pokeball-48.png"  alt="poke"/> 
-            <br/>
-            CATCH
-          </Button>
+        <CardMoves moves={pokemons.moves} />
+      </Row>
+      <Row className="justify-content-md-center">
+        <Col md={1}>
+          <div className="btn-rounded button_bottom" onClick={add}> 
+            <img src="/icons8-pokeball-48.png" className="img-circle" alt="poke"/>
+          </div>
           
-        </div>
+        </Col>
       </Row>
     </Container>
   );

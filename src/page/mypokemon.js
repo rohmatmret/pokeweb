e2ed21a46fd6707c656393ab919089d314ae5f72
rohmatmret <rohmat771@gmail.com>
2021-01-18 17:45:55 +0700

    add function and view realease

--- src/page/mypokemon.js
+++ src/page/mypokemon.js
@@ -2,7 +2,7 @@ import React, { useState,useEffect,createContext,useContext } from 'react';
 import { Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../App.css'
-import { Container,Row,Col,Card ,Button} from 'react-bootstrap'
+import { Container,Row,Col,Card ,Button, Alert} from 'react-bootstrap'
 
 import AppContext from '../AppContext.js'
 const { CartContext } = AppContext;
@@ -18,6 +18,7 @@ function MyPokemons() {
   )
   
   const [isEmpty, setEmpty] = useState(true)
+  const [isSuccessRealease, setRealease] = useState(false)
   
   useEffect(()=>{
     document.title ='My Pokemons'
@@ -35,7 +36,7 @@ function MyPokemons() {
   },[cart])
 
   function release(index){
-    console.log(index)
+    
     const cart = window.localStorage.getItem('cart')
     const storageCart = JSON.parse(cart)
     storageCart.splice(index, 1);
@@ -47,6 +48,8 @@ function MyPokemons() {
     if (storageCart.length < 1) {
       setEmpty(true)
     }
+
+    setRealease(true)
   }
 
   return (
@@ -60,37 +63,68 @@ function MyPokemons() {
             </Col>
           : null}
         </Row>
+        <Row className="justify-content-md-center mt-3 pt-2">
+          
+          { isEmpty ? 
+            <Col lg={2}>
+              <Link to="/">
+                <img src="./explore.png" className="img-rounded" alt="explore" />
+              </Link>
+            </Col>
+          : null}
+        </Row>
         <Row>
           <Col lg={12}>
             <Container>
-              <Row>
-                <Consumer>
-                  {value => value.map((el,index)=> {
-                    return (
-                      <Col md={3} key={index}>
-                        <Link
-                          to={{
-                            pathname: "/detail",
-                            search: "?name=" + el.name,
-                            state: { fromDashboard: true }
-                          }} >
-                              
-                              <Card className="mt-2 p-2 border-0 cards">
-                                <img src={url + el.id +'.png'} alt={el.name} className="rounded img-card" />
-                                <Card.Body className="p-2">
-                                  <Card.Title className="text text-center"> {el.name }</Card.Title>
-                                </Card.Body>
-                              </Card>
-                        </Link>
-                        <Button className="btn btn-block" onClick={()=>release(index)}> 
-                          <img src="/icons8-open-pokeball-48.png"  alt="poke"/> 
-                          <i> Release</i>
+              {isSuccessRealease ?
+                <>
+                  <Row className="justify-content-md-center">
+                    <Col lg={4}>
+                      <Alert  variant={'success'}>
+                        Success Realease
+                      </Alert>
+                    </Col>
+                  </Row>
+                  <Row className="justify-content-md-center">
+                    { !isEmpty ?
+                      <Col lg={2}>
+                        <Button className="btn btn-block btn-dark" onClick={()=>setRealease(false)}>
+                          Back
                         </Button>
                       </Col>
-                    )
-                  })}
-                </Consumer>
-              </Row>
+                    :null}
+                  </Row>
+                </>
+              :
+                <Row>
+                  <Consumer>
+                    {value => value.map((el,index)=> {
+                      return (
+                        <Col md={3} key={index}>
+                          <Link
+                            to={{
+                              pathname: "/detail",
+                              search: "?name=" + el.name,
+                              state: { fromDashboard: true }
+                            }} >
+                                
+                                <Card className="mt-2 p-2 border-0 cards">
+                                  <img src={url + el.id +'.png'} alt={el.name} className="rounded img-card" />
+                                  <Card.Body className="p-2">
+                                    <Card.Title className="text text-center"> {el.name }</Card.Title>
+                                  </Card.Body>
+                                </Card>
+                          </Link>
+                          <Button className="btn btn-block" onClick={()=>release(index)}> 
+                            <img src="/icons8-open-pokeball-48.png"  alt="poke"/> 
+                            Release
+                          </Button>
+                        </Col>
+                      )
+                    })}
+                  </Consumer>
+                </Row>
+              }
             </Container>
           </Col>
         </Row>

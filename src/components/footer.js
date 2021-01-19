566edfd22e8b026758b27b8b0b010bf95536252c
rohmatmret <rohmat771@gmail.com>
2021-01-19 12:52:39 +0700

    add footer

---
+++ src/components/footer.js
@@ -0,0 +1,12 @@
+function Footer() {
+
+  return (
+    <footer fixed="bottom" class="text-center text-lg-start mt-3 mb-2">
+      <div class="text-center p-3">
+        © {new Date().getFullYear()} Copyright rohmatmret
+      </div>
+    </footer>
+  )
+}
+
+export default Footer

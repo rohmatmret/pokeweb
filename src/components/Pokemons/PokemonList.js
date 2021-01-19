64a2d8c94031792b74975cf5dfb368157f8b0806
rohmatmret <rohmat771@gmail.com>
2021-01-19 12:53:34 +0700

    hot fix owned function

--- src/components/Pokemons/PokemonList.js
+++ src/components/Pokemons/PokemonList.js
@@ -1,8 +1,10 @@
+import React, { useEffect, useState} from 'react';
 import { Row, Col,Container,Card,Badge} from 'react-bootstrap'
 import { Link } from 'react-router-dom';
 
-const PokemonList =({pokemons,url,collection})=> {
-
+const PokemonList =({pokemons,url})=> {
+  const [MyCollection, setMyCollection] = useState([])
+  const cart = window.localStorage.getItem('cart')
   function myTrim(x) {
     return x.replace('https://pokeapi.co/api/v2/pokemon/','');
   }
@@ -10,13 +12,22 @@ const PokemonList =({pokemons,url,collection})=> {
   function rewrite(r){
     return r.replace('/','.png')
   }
+
+  function getID(x){
+    return x.replace('/','')
+  }
   
-  function findOwned(name){
-    console.log('find id '+ name)
-    let find = collection.filter((c => c.name === name ))
-    return find.length;
+  function findOwned(id){
+    let find = MyCollection.filter((c => c.id === parseInt(id) ))
+    return find.length
   }
 
+  useEffect(()=>{
+    if(cart){
+      let incart = JSON.parse(cart)
+      setMyCollection((e=>incart))
+    }
+  },[cart])
   return (
     <>
       <Container>
@@ -24,23 +35,22 @@ const PokemonList =({pokemons,url,collection})=> {
             {pokemons.map((el,index)=> {
               return (
                 <Col md={3} key={index}>
-                  <Link
-                    to={{
-                      pathname: "/detail",
-                      search: "?name=" + el.name,
-                      state: { fromDashboard: true }
-                    }} >
+                  <Card className="mt-2 p-2 border-0 cards">
+                    <Link
+                      to={{
+                        pathname: "/pokemon/" +getID(myTrim(el.url)),
+                        state: { fromDashboard: true }
+                      }} >
                         
-                        <Card className="mt-2 p-2 border-0 cards">
-                          <img src={url +rewrite(myTrim(el.url))} alt={el.name} className="img-rounded img-card" />
-                          <Card.Body className="p-2 text-center">
-                            <Card.Text className="text text-center capitalize"> {el.name }</Card.Text>
-                            <Card.Text className="text text-center">
-                              <Badge className="badge badge-light"> OWNED  {findOwned(el.name)} </Badge>
-                            </Card.Text>
-                          </Card.Body>
-                        </Card>
-                    </Link>
+                        <img src={url +rewrite(myTrim(el.url))} alt={el.name} className="img-rounded img-card" />
+                      </Link>
+                      <Card.Body className="p-2 text-center">
+                        <Card.Text className="text text-center capitalize"> {el.name }</Card.Text>
+                        <Card.Text className="text text-center">
+                          <Badge className="badge badge-light"> OWNED  {findOwned(getID(myTrim(el.url)))} </Badge>
+                        </Card.Text>
+                      </Card.Body>
+                    </Card>
                 </Col>
               )
             })}

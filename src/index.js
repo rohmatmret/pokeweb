9783a54ea519548ef55379eabb893881c4e20df1
rohmatmret <rohmat771@gmail.com>
2021-01-19 15:21:58 +0700

    service worker

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <BrowserRouter>
@@ -15,3 +16,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+serviceWorker.unregister();
